7da39911f6d285bef245247db91b8842910a9443
sonelin09 <sonelin0309@gmail.com>
2022-06-06 23:34:42 +0800

    按鈕加入onclick事件，讀取input的數字，製作對應的宮格數

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ const askText = document.createElement('h1');
 askText.innerText = '請輸入想要幾X幾的畫布：';
 document.body.appendChild(askText);
 
+//輸入框跟確定鈕
 const askDiv = document.createElement('div');
 document.body.appendChild(askDiv);
 const enterBox = document.createElement('input');
 askDiv.appendChild(enterBox);
 const enterBtn = document.createElement('button');
+enterBtn.innerText = '確定';
 askDiv.appendChild(enterBtn);
+
+enterBtn.onclick = getNumber;
+
 
 //一個容器用來裝需要的divs
 let container = document.createElement('div');
@@ -15,17 +20,33 @@ container.setAttribute('class', 'container');
 document.body.appendChild(container);
 
 //創造divs放進容器
-for (i = 0; i< 16*16; i++) {
+/*for (i = 0; i< 16*16; i++) {
     let cell = document.createElement('div');
     cell.innerText = (i + 1);
     container.appendChild(cell).className = 'grid-item';
+}*/
+
+//判斷輸入框的值
+function getNumber() {
+    if(enterBox.value %1 === 0 && enterBox.value != ''){
+        console.log('整數');
+        container.style = `grid-template-columns: repeat(${enterBox.value}, 1fr)`;
+        for (i = 0; i< enterBox.value*enterBox.value; i++) {
+            let cell = document.createElement('div');
+            cell.innerText = (i + 1);
+            container.appendChild(cell).className = 'grid-item';
+        }
+
+        let cells = document.getElementsByClassName('grid-item');
+        for (k = 0; k < cells.length; k++) {
+            cells[k].addEventListener('mouseenter', changeColor)
+        }
+
+    }else {
+        console.log('不是整數');
+    }
 }
 
-//給divs加上事件監聽器
-let cells = document.getElementsByClassName('grid-item');
-for (k = 0; k < cells.length; k++) {
-    cells[k].addEventListener('mouseenter', changeColor)
-}
 
 //改顏色的函式
 function changeColor(e) {
